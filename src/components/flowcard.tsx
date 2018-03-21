982d5f3907f08c9b849ae88b8e7ff34e528f9df1
wimjjj <wim.jongeneel1@gmail.com>
2018-03-21 15:20:23 +0100

    add flowchard renderer

---
+++ src/components/flowcard.tsx
@@ -0,0 +1,33 @@
+import * as React from "react"
+import { Row, Card, Col } from "antd"
+
+type Block = SimpleBlock | BranchedBlock
+
+type SimpleBlock = {
+  kind: "simple-block"
+  value: string
+  block: Block | "none"
+}
+
+type BranchedBlock = {
+  kind: "branched-block"
+  value: string
+  block: Block | "none"
+  branche: Block | "none"
+}
+
+const renderBlockValue = (block: Block) => (
+  <Card>
+    <h2>{block.value}</h2>
+  </Card>
+)
+
+export const renderBlock = (block: Block, colum = 0, row = 0) => (
+  <div>
+    <div style={{ position: "absolute", left: colum * 300 + .05*window.innerHeight, top: row * 150 + 0.15*window.innerHeight, width: 200, height: 100 }}>
+      {renderBlockValue(block)}
+    </div>
+    {block.block != "none" && renderBlock(block.block, colum + 1, row)}
+    {block.kind == "branched-block" && block.branche != "none" && renderBlock(block.branche, colum, row + 1)}
+  </div>
+)
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,39 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {renderBlock} from "./components/flowcard"
+import { Layout, Row } from "antd"
 
-const App = () => <h1>Hello World</h1>
+const App = () => (
+  <Layout>
+    <Layout.Header style={{ height: "10vh" }}></Layout.Header>
+    <Layout style={{ height: "70vh"}}>
+      {renderBlock({
+        kind: "simple-block",
+        value: "button is pressed",
+        block: {
+          kind: "branched-block",
+          value: "if x  <3",
+          block: {
+            kind: "simple-block",
+            value: "turn LED on",
+            block: "none"
+          },
+          branche: {
+            kind: "simple-block",
+            value: "turn LED off",
+            block: {
+              kind: "simple-block",
+              value: "repeat programm",
+              block: "none"
+            }
+          }
+        }
+      })}
+      <Layout.Footer style={{ height: "20vh"}}>
+      </Layout.Footer>
+      </Layout>
+  </Layout>
+)
 
 
 ReactDOM.render(
