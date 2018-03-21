10324fcc71b879cf415269882ce9dc13bd7cee14
wimjjj <wim.jongeneel1@gmail.com>
2018-03-21 15:35:50 +0100

    add Layout

--- src/index.tsx
+++ src/index.tsx
@@ -3,37 +3,67 @@ import * as ReactDOM from "react-dom";
 import {renderBlock} from "./components/flowcard"
 import { Layout, Row } from "antd"
 
-const App = () => (
-  <Layout>
-    <Layout.Header style={{ height: "10vh" }}></Layout.Header>
-    <Layout style={{ height: "70vh"}}>
-      {renderBlock({
-        kind: "simple-block",
-        value: "button is pressed",
-        block: {
-          kind: "branched-block",
-          value: "if x  <3",
-          block: {
+class App extends React.Component<{}, {}> {
+
+  render() {
+    return (
+      <Layout>
+        <Layout.Header style={{ height: "10vh", position: "fixed", top: 0, left: 0, width: "100vw" }}></Layout.Header>
+        <Layout style={{ height: "70vh"}}>
+          {renderBlock({
             kind: "simple-block",
-            value: "turn LED on",
-            block: "none"
-          },
-          branche: {
-            kind: "simple-block",
-            value: "turn LED off",
+            value: "button is pressed",
             block: {
-              kind: "simple-block",
-              value: "repeat programm",
-              block: "none"
+              kind: "branched-block",
+              value: "if x  <3",
+              block: {
+                kind: "simple-block",
+                value: "turn LED on",
+                block: "none"
+              },
+              branche: {
+                kind: "simple-block",
+                value: "turn LED off",
+                block: {
+                  kind: "simple-block",
+                  value: "repeat programm",
+                  block: {
+                    kind: "simple-block",
+                    value: "repeat programm",
+                    block: {
+                      kind: "branched-block",
+                      value: "if x  <3",
+                      block: {
+                        kind: "simple-block",
+                        value: "turn LED on",
+                        block: "none"
+                      },
+                      branche: {
+                        kind: "simple-block",
+                        value: "turn LED off",
+                        block: {
+                          kind: "simple-block",
+                          value: "repeat programm",
+                          block: {
+                            kind: "simple-block",
+                            value: "repeat programm",
+                            block: "none"
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
             }
-          }
-        }
-      })}
-      <Layout.Footer style={{ height: "20vh"}}>
-      </Layout.Footer>
+          })}
+          <Layout.Footer style={{ position: "fixed", height: "20vh", width: "100vw", bottom: 0, left: 0, backgroundColor: "white" }}>
+          </Layout.Footer>
+          </Layout>
       </Layout>
-  </Layout>
-)
+    )
+  }
+}
 
 
 ReactDOM.render(
